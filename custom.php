d703bfaa6b281687c0db01239b2a05e697e5f5f7
chrismooreproductions <chrismooreproductions@gmail.com>
2017-08-06 16:26:09 +0100

    Corrected the package.json for owl carousel 2

--- custom.php
+++ custom.php
@@ -14,13 +14,13 @@ function register_products_menu() {
 add_action( 'init', 'register_products_menu' );
 
 function enqueue_npm_scripts() {
-    wp_enqueue_style( 'animate', '/node_modules/animate.css/animate.min.css' );
-    wp_enqueue_style( 'owl', '/node_modules/owl-carousel-2/assets/owl.carousel.min.css' );
-    wp_enqueue_style( 'owl-theme', '/node_modules/owl-carousel-2/assets/owl.theme.default.min.css' );
-    wp_enqueue_style( 'hover', '/node_modules/hover.css/css/hover-min.css' );
+    wp_enqueue_style( 'animate', plugin_dir_url( __FILE__ ).'/node_modules/animate.css/animate.min.css' );
+    wp_enqueue_style( 'owl', plugin_dir_url( __FILE__ ).'/node_modules/owl-carousel-2/assets/owl.carousel.min.css' );
+    wp_enqueue_style( 'owl-theme', plugin_dir_url( __FILE__ ).'/node_modules/owl-carousel-2/assets/owl.theme.default.min.css' );
+    wp_enqueue_style( 'hover', plugin_dir_url( __FILE__ ).'/node_modules/hover.css/css/hover-min.css' );
 
-    wp_enqueue_script( 'owl-carousel-js', '/node_modules/owl-carousel-2/owl.carousel.min.js', array( 'jquery' ), '1.0.0', true );
-    wp_enqueue_script( 'parallax-js', '/node_modules/parallax-js/dist/parallax.min.js', array( 'jquery' ), '1.0.0', true );
-    wp_enqueue_script( 'owl-init-home.js', '/js/owl-init-home.js', array( 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'owl-carousel-js', plugin_dir_url( __FILE__ ).'/node_modules/owl-carousel-2/owl.carousel.min.js', array( 'jquery' ), '1.0.0', true );
+    wp_enqueue_script( 'parallax-js', plugin_dir_url( __FILE__ ).'/node_modules/parallax-js/dist/parallax.min.js', array( 'jquery' ), '1.0.0', true );
+    // wp_enqueue_script( 'owl-init-home.js', '/js/owl-init-home.js', array( 'jquery' ), '1.0.0', true );
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_npm_scripts' );
